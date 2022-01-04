9e8c7d3e3b58af56ab3e19ccb0235480827b3d96
Konrad Gądek <kgadek@gmail.com>
2022-01-04 15:38:44 +0100

    Rename git branch: master -> main |2

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,15 +2,15 @@
 set -euxo pipefail
 
 export DEVELOP_BRANCH="develop"
-export MASTER_BRANCH="main"
+export MAIN_BRANCH="main"
 
-export MASTER_HASH="$(git rev-parse "${MASTER_BRANCH}")"
+export MAIN_HASH="$(git rev-parse "${MAIN_BRANCH}")"
 export DEVELOP_HASH="$(git rev-parse "${DEVELOP_BRANCH}")"
 export DEVELOP_HASH_SHORT="$(git rev-parse --short "${DEVELOP_BRANCH}")"
 export DEVELOP_COMMITDATE="$(git log -1 --format=%cd "${DEVELOP_BRANCH}")"
 export DEVELOP_PUBLIC_TREE="$(git rev-parse "${DEVELOP_BRANCH}^{tree}:public/")"
-export NEW_COMMIT="$(git commit-tree -m "Deploy from ${DEVELOP_BRANCH} SHA1:${DEVELOP_HASH_SHORT} (${DEVELOP_COMMITDATE})" -p "${MASTER_HASH}" -p "${DEVELOP_HASH}" "${DEVELOP_PUBLIC_TREE}")"
+export NEW_COMMIT="$(git commit-tree -m "Deploy from ${DEVELOP_BRANCH} SHA1:${DEVELOP_HASH_SHORT} (${DEVELOP_COMMITDATE})" -p "${MAIN_HASH}" -p "${DEVELOP_HASH}" "${DEVELOP_PUBLIC_TREE}")"
 
-git checkout "${MASTER_BRANCH}"
+git checkout "${MAIN_BRANCH}"
 git reset --hard "${NEW_COMMIT}"
 
